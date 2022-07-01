921f0bbc57cd31a0a162e078403d659e8cab2ff9
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2022-07-01 13:17:18 +0900

    Add experiment headers struct, fix skip firstline bug

--- src/LVM.jl
+++ src/LVM.jl
@@ -1,73 +1,107 @@
 module LVM
 
-function read(file::String; getall::Bool=false)
+export loadexperiment,
+       experimentschemes
 
-    headerindex = Int[]
+
+"""
+This idea for loading different headers for different experiments
+comes from ColorSchemes.jl.
+"""
+struct ExperimentScheme{V <: AbstractVector{<:String}}
+    headers::V
+end
+
+const experimentschemes = Dict{Symbol, ExperimentScheme}()
+
+function loadexperiment(experiment, headers)
+    haskey(experimentschemes, experiment) && println("$experiment overwritten")
+    experimentschemes[experiment] = LVM.ExperimentScheme(headers)
+    return experimentschemes[experiment]
+end 
+
+function loadallexperiments()
+    datadir = joinpath(dirname(@__DIR__), "data")
+    include(joinpath(datadir, "experiments.jl"))
+
+    # create experiment schemes as constants
+    for key in keys(experimentschemes)
+        @eval const $key = experimentschemes[$(QuoteNode(key))]
+    end
+end
+
+loadallexperiments()
+
+
+function read(file, experiment)
+
+    headerindices = Int[]
     headers = []
-    data = Dict()
+    data = Dict{Any, Vector{Float64}}()
 
     for (i, line) in enumerate(eachline(file))
-        
-        first_item = "1.0"
-        t_index = findfirst('\t', line)
-        r_index = findfirst('\r', line)
 
-        if t_index === nothing
-            if !(r_index === nothing)
-                first_item = line[1:r_index]
-            end
-        elseif !(t_index === nothing)
-            first_item = line[1:t_index]
+        first_item = "1"
+        first_tab = findfirst('\t', line)
+        first_return = findfirst('\r', line)
+
+        if !(first_tab === nothing)
+            first_item = line[1:first_tab]
+            
+        elseif !(first_return === nothing)
+            first_item = line[1:first_return]
         end
 
         if tryparse(Float64, first_item) === nothing
 
-            if occursin('\r', line)
+            if occursin("\t\r", line)
+                header = split(line[1:findfirst("\t\r", line)[1]])
+
+            elseif occursin('\r', line)
                 header = split(line[1:findfirst('\r', line)])
-            else
-                header = split(line, '\t')
             end
-            push!(headerindex, i)
+            push!(headerindices, i)
             push!(headers, header)
         end
     end
-  
-    for (i, h) in enumerate(headerindex)
 
-        if headerindex[i] == headerindex[end]
-            lines = readlines(file)[h + 1:end]
+    lines = readlines(file)
+    for (i, headerindex) in enumerate(headerindices)
+
+        # Because of the strange placement of '\r' and '\t\r'
+        # characters in the headers, we have to separately grab the first line of data.
+        # Otherwise, the first row of data is part of the header.
+        # Then we can add the rest of the data in a loop, skipping the first row.
+
+        firstline = lines[headerindex][findfirst('\r', lines[headerindex]):end]
+
+        if headerindex == last(headerindices)
+            datalines = lines[headerindex + 1:end]
         else
-            lines = readlines(file)[h + 1 : headerindex[i + 1] - 1]
+            datalines = lines[headerindex + 1 : headerindices[i+1] - 1]
         end
 
-        colsize = length(split(lines[1]))
-        chunk = Array{Float64}(undef, length(lines), colsize)
+        # We need the +1 row for the first line.
+        chunk = Array{Float64}(undef, length(datalines) + 1, length(headers[i]))
 
-        for (i, line) in enumerate(lines)
-            for (j, item) in enumerate(split(line))
-                chunk[i, j] = parse(Float64, item)
+        chunk[1, :] = [parse(Float64, x) for x in split(firstline, '\t')]
+
+        for (j, line) in enumerate(datalines)
+            for (k, item) in enumerate(split(line))
+                chunk[j+1, k] = parse(Float64, item)
             end
         end
 
-        for (k, header) in enumerate(headers[i])
+        for (h, header) in enumerate(headers[i])
 
-            if occursin("wavelength", header)
-                data["wavelength"] = chunk[:, k]
-
-            elseif occursin("wavenum", header)
-                data["wavenumber"] = chunk[:, k]
-            elseif occursin("Time", header)
-                data["time"] = chunk[:, k]
-
-            elseif getall == false
-                if occursin("CH0_diff", header)
-                    data["diffsignal"] = chunk[:, k]
-                elseif occursin("CH0_", header)
-                    data["signal"] = chunk[:, k]
+            for eheader in experimentschemes[experiment].headers
+                if occursin(String(eheader), header)
+                    data[lowercase(String(eheader))] = chunk[:, h]
+                else
+                    data[header] = chunk[:, h]
                 end
-            else
-                data[header] = chunk[:, k]
             end
+
         end
     end
     
