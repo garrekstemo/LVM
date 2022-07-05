ef345392ce66be3183ad502167bc5dd335fe2c68
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2022-07-05 17:18:25 +0900

    Fixed bug with CH0 occuring multiple times

--- data/experiments.jl
+++ data/experiments.jl
@@ -2,6 +2,7 @@ loadexperiment(:MIR,
     Dict("Time" => "time", 
          "wavelength" => "wavelength", 
          "wavenum" => "wavenumber", 
-         "CH0_" => "signal", 
+         "CH0_ON_" => "on",
+         "CH0_OFF_" => "off",
          "CH0_diff_" => "ΔA")
 )
