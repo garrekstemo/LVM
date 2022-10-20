85de21d71090ca7f143cef0979d25ecf59dd069d
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2022-10-20 19:13:07 +0900

    Change Delta A to Delta

--- data/experiments.jl
+++ data/experiments.jl
@@ -1,8 +1,8 @@
 loadexperiment(:MIR, 
     Dict("Time" => "time", 
          "wavelength" => "wavelength", 
-         "wavenum" => "wavenumber", 
+         "wavenum" => "wavenumber",
          "CH0_ON_" => "on",
          "CH0_OFF_" => "off",
-         "CH0_diff_" => "ΔA")
+         "CH0_diff_" => "Δ")
 )
