b22e675a3fbe7b890976f2c6a00523a0bd856380
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2023-05-31 14:56:43 +0900

    MIRFile struct to hold raw data

--- src/LVM.jl
+++ src/LVM.jl
@@ -6,7 +6,54 @@ export loadexperiment,
        lvm_to_df
 
 using DataFrames
+using Dates
 
 include("functions.jl")
+
+
+"""
+    MIRFile
+
+A mutable struct for storing MIR data.
+
+Fields:
+date::String
+time::String
+sample::String
+pumpdelay::Int64
+gratingwavelength::Int64
+wavenumber::Vector{Float64}
+wavelength::Vector{Float64}
+time::Vector{Float64}
+ΔA::Vector{Float64}
+
+Units:
+pumpdelay is in fs.
+gratingwavelength is in nm.
+wavenumber is in cm^-1.
+wavelength is in nm.
+time is in fs.
+"""
+mutable struct MIRFile
+    datetime::DateTime          # DateTime type
+    sample::String              # sample information
+    pumpdelay::Int64            # fs
+    gratingwavelength::Int64    # nm
+    wavenumber::Vector{Float64} # cm^-1
+    wavelength::Vector{Float64} # nm
+    fs::Vector{Float64}         # fs
+    ΔA::Vector{Float64} 
+
+    function MIRFile(date::String, time::String, sample::String, pumpdelay::Int64, gratingwavelength::Int64, wavenumber::Vector{Float64}, wavelength::Vector{Float64}, time::Vector{Float64}, ΔA::Vector{Float64})
+        new(date, time, sample, pumpdelay, gratingwavelength, wavenumber, wavelength, time, ΔA)
+    end
+
+    function MIRFile(filename)
+        datetime = get_datetime(filename)
+        dict = readlvm(filename, :MIR)
+        fields = names(dict)
+    # end
+
+end
 
 end # module
